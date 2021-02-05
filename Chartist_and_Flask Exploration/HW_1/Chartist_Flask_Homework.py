7d1f0037e29f36e571a9507d28fdf30d5b668586
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-04 22:59:56 -0500

    [wip] trying to figure out hw how to convert dataset

--- Chartist_and_Flask Exploration/HW_1/Chartist_Flask_Homework.py
+++ Chartist_and_Flask Exploration/HW_1/Chartist_Flask_Homework.py
@@ -9,8 +9,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Attach our dataframe to our app
-app.df = pd.read_csv("multiTimeline.csv", skiprows=1)
-app.df.columns = ["month", "diet", "gym", "finance"]
+app.df = pd.read_csv("all_stocks_5yr.csv", skiprows=1)
+# app.df.columns = ["date", "close", "Name"]
 
 
 @app.route("/", methods=["GET"])
@@ -30,21 +30,34 @@ def get_time_series_data():
     ls_year = [int(year) for year in request.args.getlist("n")]
     ls_col = request.args.getlist("m")
 
+    df_columns = ["date", "close", "Name"]
+
     # Generate a list of all the months we need to get
     all_years = [str(year) for year in range(min(ls_year), max(ls_year) + 1)]
 
-    # Grab all of the wanted months by filtering for the ones we want
+    # Grab all of the wanted dates by filtering for the ones we want
     wanted_months = reduce(
-        lambda a, b: a | b, (app.df["month"].str.contains(year) for year in all_years)
+        lambda a, b: a | b, (app.df["date"].str.contains(year) for year in all_years)
     )
 
+    #get wanted days
+
+    print("LS COL:", ls_col)
     # Create a new dataframe from the one that
-    df_new = app.df[wanted_months][["month"] + ls_col]
+    # df_new = app.df[wanted_months][["date"] + ls_col]
+    df_new = app.df[wanted_months][["date"] + ["close"]]
+
+    print("NEW DF with + ", df_new.to_json())
 
     # Convert all string dates into datetime objects and then sort them
-    df_new["month"] = pd.to_datetime(df_new["month"])
-    df_new = df_new.sort_values(by=["month"])
+    df_new["date"] = pd.to_datetime(df_new["date"])
+    df_new = df_new.sort_values(by=["date"])
 
+    df_new = df_new[df_columns] #select right columns
+
+    #now select the right rows
+    df_new = df_new[df_columns["Name"] == "AAPL"]
+    print("New DF:", df_new)
     # Return the dataframe as json
     return df_new.to_json(), 200
 
